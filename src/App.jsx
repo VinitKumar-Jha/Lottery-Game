6a5075175ff8049f081ff3ba6ec05b54ec256820
Vinit Kumar Jha <vinitkumarjha7739@gmail.com>
2025-10-14 09:34:45 +0530

    Import Lottery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import Lottery from "./Lottery"
 import './App.css'
-import Ticket from "./Ticket"
 
 function App() {
 
   return (
     <>
-      {/* <Lottery /> */}
-      <Ticket ticket={[0, 1, 2]} />   
-       <Ticket ticket={[0, 1, 2, 4, 5]} />
-
-
+      <Lottery n={4} winningSum={15} />
     </>
   )
 }
--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,24 +1,19 @@
 import { useState } from "react"
-import "./Lottery.css"
 import { genTicket, sum } from "./helper"
+import Ticket from "./Ticket";
 
-export default function Lottery() {
+export default function Lottery({ n, winningSum }) {
 
-    let [ticket, setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) === 15;
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = sum(ticket) === winningSum;
 
     let buyTicket = () => {
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     }
     return (
         <div>
             <h2>Lottery Game!</h2>
-            <div className="tickets">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br />
+            <Ticket ticket={ticket}/>
             <button onClick={buyTicket}>Buy New Ticket</button>
             <h3>{isWinning && "Congratulations, You won!"}</h3>
         </div>
